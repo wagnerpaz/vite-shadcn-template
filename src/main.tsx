2c104af16c2926dde8b9f6f4456b0c9b0a1684a3
Wagner Paz <wagner.o.paz@gmail.com>
2025-12-16 17:10:22 -0300

    add a button using the cli to demonstrate it's working

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from '@/components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 className="text-red-500 font-bold">Red Title</h1>
+    <Button variant="outline" className="m-2">Shadcn Working!</Button>
   </StrictMode>,
 )
